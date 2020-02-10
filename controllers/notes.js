d304158c4054e7b96d6828ebd023beb4d3e7e017
Sean Parkin <sdparkin@gmail.com>
2020-02-09 18:22:33 -0600

    testing something

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,7 +6,7 @@ const { noteValidation } = require("../validation");
 // @access Public
 exports.getNotes = async (req, res, next) => {
   try {
-    const notes = await Note.find({}, null, { sort: { custName: asc } });
+    const notes = await Note.find();
 
     return res.status(200).json({
       success: true,
