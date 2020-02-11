f692c83927409aca72d56a37fc84aba2cbd4edb7
Sean Parkin <sdparkin@gmail.com>
2020-02-10 20:10:38 -0600

    Attempting to sort the list of stops

--- controllers/notes.js
+++ controllers/notes.js
@@ -6,7 +6,7 @@ const { noteValidation } = require("../validation");
 // @access Public
 exports.getNotes = async (req, res, next) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({ custName: 1 });
 
     return res.status(200).json({
       success: true,
