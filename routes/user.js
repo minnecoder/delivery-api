a7824852ef093294511a4376471ad92178e8746b
Sean Parkin <sdparkin@gmail.com>
2020-03-22 09:31:26 -0500

    Small change

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const verify = require('../auth/verifyToken');
+// const verify = require('../auth/verifyToken');
 const { loginUser, addUser, getUser } = require('../controllers/user');
 
 const router = express.Router();
 
 // router.route('/add').post(verify, addUser);
-router.route('/add').post(verify, addUser);
+router.route('/add').post(addUser);
 router.route('/login').post(loginUser);
 router.route('/:userName').get(getUser);
 module.exports = router;
