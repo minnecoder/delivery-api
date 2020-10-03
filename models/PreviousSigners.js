9436a640c4c2c7bf519064adf7f2fd20a4124475
minnecoder <sdparkin@protonmail.com>
2020-10-03 00:00:32 -0500

    Added PreviousSigners model

---
+++ models/PreviousSigners.js
@@ -0,0 +1,31 @@
+const Sequelize = require("sequelize");
+const db = require("../config/postgres-db");
+const Customer = require("./Customer");
+
+const PreviousSigners = db.define("previous_signers", {
+  customer_id: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Customer,
+      key: "id",
+      deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+    }
+  },
+  first_name: {
+    type: Sequelize.STRING
+  },
+  last_name: {
+    type: Sequelize.STRING
+  },
+  createdAt: {
+    type: Sequelize.DATE
+  },
+  updatedAt: {
+    type: Sequelize.DATE
+  }
+});
+
+Customer.hasMany(PreviousSigners);
+PreviousSigners.belongsTo(Customer);
+
+module.exports = PreviousSigners;
