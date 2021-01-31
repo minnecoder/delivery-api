0cc58cd8278e0662c858d67c3d56222543183598
minnecoder <sdparkin@protonmail.com>
2021-01-30 19:25:34 -0600

    Fixed issues with column names

--- models/OrderItem.js
+++ models/OrderItem.js
@@ -3,33 +3,39 @@ const db = require("../config/postgres-db");
 const Order = require("./Order");
 const Product = require("./Product");
 
-const OrderItems = db.define("order_items", {
-  order_id: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: Order,
-      key: "id",
-      deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+const OrderItems = db.define(
+  "order_items",
+  {
+    order_id: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Order,
+        key: "id",
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    },
+    product_id: {
+      type: Sequelize.INTEGER,
+      references: {
+        model: Product,
+        key: "id",
+        deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+      }
+    },
+    quantity: {
+      type: Sequelize.INTEGER
+    },
+    created_at: {
+      type: Sequelize.DATE
+    },
+    updated_at: {
+      type: Sequelize.DATE
     }
   },
-  product_id: {
-    type: Sequelize.INTEGER,
-    references: {
-      model: Product,
-      key: "id",
-      deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
-    }
-  },
-  quantity: {
-    type: Sequelize.INTEGER
-  },
-  createdAt: {
-    type: Sequelize.DATE
-  },
-  updatedAt: {
-    type: Sequelize.DATE
+  {
+    underscored: true
   }
-});
+);
 
 Order.hasMany(OrderItems);
 OrderItems.belongsTo(Order);
